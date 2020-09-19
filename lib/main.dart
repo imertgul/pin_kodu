a129f7e7fd503fd7e962c19355681aeae80e1476
Mert Gul <ibrahimmertgul@gmail.com>
2020-09-19 13:24:54 +0300

    debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Pin Konuna Bak',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.grey,
         visualDensity: VisualDensity.adaptivePlatformDensity,
