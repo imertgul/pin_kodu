cf1ac743f34a66cbef3d42013d6c11f8c113fc27
Mert Gül <ibrahimmertgul@gmail.com>
2020-09-19 12:09:05 +0300

    Create constant.dart

---
+++ lib/utilities/constant.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+TextStyle myStyle25 = new TextStyle(fontSize: 25);
+TextStyle myStyle20 = new TextStyle(fontSize: 20);
+TextStyle myStyle15 = new TextStyle(fontSize: 15);
+TextStyle myStyle10 = new TextStyle(fontSize: 10);
+
+List<int> myBirthdate = List<int>.generate(8, (index) => 0);
