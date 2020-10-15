b72141f6e7c9b1ba0fa741fcb27ae8b75a6a5d06
Mert Gül <ibrahimmertgul@gmail.com>
2020-10-15 20:44:34 +0300

    Çift input ekranı oluşturuldu

---
+++ lib/page/CouplePage.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:pin_kodu/utilities/constant.dart';
+
+class CouplePage extends StatefulWidget {
+  final String title;
+
+  CouplePage({Key key, @required this.title}) : super(key: key);
+  @override
+  _CouplePageState createState() => _CouplePageState();
+}
+
+class _CouplePageState extends State<CouplePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: <Widget>[
+            Align(
+              alignment: Alignment.topLeft,
+              child: FlatButton(
+                  onPressed: () => Navigator.pop(context), child: Text("Geri")),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                buildTitle(widget.title),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    "Lütfen Doğum tarihlerinizi giriniz",
+                    style: myStyle15,
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    buildInput(context, myLadydate, 0, myLadyController),
+                    buildInput(context, myLadydate, 1, myLadyController),
+                    SizedBox(width: 5),
+                    buildInput(context, myLadydate, 2, myLadyController),
+                    buildInput(context, myLadydate, 3, myLadyController),
+                    SizedBox(width: 5),
+                    buildInput(context, myLadydate, 4, myLadyController),
+                    buildInput(context, myLadydate, 5, myLadyController),
+                    buildInput(context, myLadydate, 6, myLadyController),
+                    buildInput(context, myLadydate, 7, myLadyController),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    buildInput(context, myLadydate, 0, myManController),
+                    buildInput(context, myLadydate, 1, myManController),
+                    SizedBox(width: 5),
+                    buildInput(context, myLadydate, 2, myManController),
+                    buildInput(context, myLadydate, 3, myManController),
+                    SizedBox(width: 5),
+                    buildInput(context, myLadydate, 4, myManController),
+                    buildInput(context, myLadydate, 5, myManController),
+                    buildInput(context, myLadydate, 6, myManController),
+                    buildInput(context, myLadydate, 7, myManController),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    FlatButton(
+                      onPressed: () {
+                        FocusScope.of(context).unfocus();
+
+                        for (var i = 0; i < myController.length; i++) {
+                          myController[i].clear();
+                        }
+                      },
+                      child: Text("Sıfırla", style: myStyle15),
+                    ),
+                    FlatButton(
+                      onPressed: () {
+                        myPinCode = calculatePin(myBirthdate);
+                        print('My Birthdate: $myBirthdate');
+                        print('My Pincode: $myPinCode');
+                        // Navigator.push(
+                        //   context,
+                        //   MaterialPageRoute(builder: (context) => ResultPage()),
+                        // );
+                      },
+                      child: Text(
+                        "Hesapla",
+                        style: myStyle15,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
